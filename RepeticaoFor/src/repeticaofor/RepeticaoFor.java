6b7afb6db64c12bc12949ec6bdba9e97c752a4fb
b3lzebuth <leandrofelipecunha@hotmail.com>
2021-01-14 13:31:05 -0300

    estrutura for

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,12 @@
+package repeticaofor;
+
+public class RepeticaoFor {
+    public static void main(String[] args) {
+        for (int c=0; c<=3; c++){
+            for (int d=0; d<=2; d++){
+                System.out.println(c + " " + d);
+            }
+        }
+    }
+    
+}
